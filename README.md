b12d743f5c96334ae7209febf69f7db085e194e9
Vlad <vlad.kosevichhhh@gmail.com>
2024-03-27 16:27:56 +0300

    upload interface

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# ecommerce-shop
+# Full Stack E-Commerce + Dashboard & CMS: Next.js 13 App Router, React, Tailwind, Prisma, MySQL
+
+### Prerequisites
+
+**Node version 14.x**
+
+### Install packages
+
+```shell
+npm i
+```
+
+### Setup .env file
+
+```js
+NEXT_PUBLIC_API_URL=
+```
+
+### Start the app
+
+```shell
+npm run dev
+```
+
+## Available commands
+
+Running commands with npm `npm run [command]`
+
+| command | description                              |
+| :------ | :--------------------------------------- |
+| `dev`   | Starts a development instance of the app |
---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import { Urbanist } from "next/font/google";
+
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
+import ModalProvider from "@/providers/modal-provider";
+import ToastProvider from "@/providers/toast-provider";
+
+import "./globals.css";
+
+const font = Urbanist({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Desk Culture | Shop",
+  description:
+    "Create a workspace that epitomizes your refined preferences while igniting your drive for productivity.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ToastProvider />
+        <ModalProvider />
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
