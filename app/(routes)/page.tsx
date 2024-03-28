b644a84aa59a6c59066074905bc73ddf9d67af82
Vlad <vlad.kosevichhhh@gmail.com>
2024-03-28 13:38:29 +0300

    Change prod list title

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -15,7 +15,7 @@ const HomePage = async () => {
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-          <ProductList title="Best seller" items={products} />
+          <ProductList title="Featured collection" items={products} />
         </div>
       </div>
     </Container>
