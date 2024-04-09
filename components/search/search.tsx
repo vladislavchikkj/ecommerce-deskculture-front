e9538a35d2a93b88ed11aed5c45c42ed5523de1c
Vlad <vlad.kosevich@mail.ru>
2024-04-09 18:38:43 +0300

    fix layout

--- components/search/search.tsx
+++ components/search/search.tsx
@@ -37,7 +37,7 @@ const Search: React.FC<ProductListProps> = ({ items }) => {
     <NavigationMenu.Root className="relative z-[1] flex w-full justify-end sm:pr-5">
       <NavigationMenu.List className="center m-0 flex list-none rounded-[6px] p-1 ">
         <NavigationMenu.Item className="flex">
-          <NavigationMenu.Trigger className="text-violet11  focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
+          <NavigationMenu.Trigger className="text-violet11  focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px] ">
             <p className="hidden sm:block text-sm pr-2">Search</p>
             <SearchIcon width={20} />
             <CaretDownIcon
@@ -45,7 +45,7 @@ const Search: React.FC<ProductListProps> = ({ items }) => {
               aria-hidden
             />
           </NavigationMenu.Trigger>
-          <NavigationMenu.Content className="data-[motion=from-start]:animate-enterFromLeft data-[motion=from-end]:animate-enterFromRight data-[motion=to-start]:animate-exitToLeft data-[motion=to-end]:animate-exitToRight sm:w-auto sm:h-full">
+          <NavigationMenu.Content className="data-[motion=from-start]:animate-enterFromLeft data-[motion=from-end]:animate-enterFromRight data-[motion=to-start]:animate-exitToLeft data-[motion=to-end]:animate-exitToRight sm:w-auto sm:h-full border">
             <ul className="m-0 grid list-none gap-x-[10px] p-[22px] sm:w-full sm:grid-cols-[0.75fr_2fr] items-start relative">
               <li className="row-span-3 hidden sm:grid">
                 <NavigationMenu.Link asChild>
@@ -103,7 +103,7 @@ const Search: React.FC<ProductListProps> = ({ items }) => {
       </NavigationMenu.List>
 
       <div className="perspective-[2000px] absolute top-full w-max sm:left-0 flex sm:w-full justify-end left-[-8.75rem]">
-        <NavigationMenu.Viewport className="w-[85vw] data-[state=open]:animate-scaleIn data-[state=closed]:animate-scaleOut relative mt-[10px] h-[var(--radix-navigation-menu-viewport-height)] origin-[top_center] overflow-hidden rounded-[6px] bg-white transition-[width,_height] duration-300 sm:w-[var(--radix-navigation-menu-viewport-width)]" />
+        <NavigationMenu.Viewport className="w-[85vw] data-[state=open]:animate-scaleIn data-[state=closed]:animate-scaleOut relative mt-[10px] h-[var(--radix-navigation-menu-viewport-height)] origin-[top_center] overflow-hidden rounded-[6px] bg-white transition-[width,_height] duration-300 sm:w-[var(--radix-navigation-menu-viewport-width)] shadow-md" />
       </div>
     </NavigationMenu.Root>
   );
