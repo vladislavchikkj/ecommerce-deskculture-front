b538c9eb90d91f693f619e92ffdab605970796b2
Vlad <vlad.kosevichhhh@gmail.com>
2024-04-12 10:50:52 +0300

    fix lay

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,10 @@ import Footer from "@/components/footer";
 import Navbar from "@/components/navbar/navbar";
 import ModalProvider from "@/providers/modal-provider";
 import ToastProvider from "@/providers/toast-provider";
-import "@radix-ui/themes/styles.css";
 
 import { ThemeProvider } from "next-themes";
 import dynamic from "next/dynamic";
 import "./globals.css";
-import { Theme } from "@radix-ui/themes";
 
 const font = Urbanist({ subsets: ["latin"] });
 
